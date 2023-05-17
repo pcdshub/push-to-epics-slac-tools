b90ff9d28255418e8daf91ca9e8fe79dacba7fc1
Ken Lauer <klauer@slac.stanford.edu>
2023-05-17 09:59:25 -0700

    REF: single push with pre-check for invalid commits

--- push2slac-epics-via-ssh.sh
+++ push2slac-epics-via-ssh.sh
@@ -7,9 +7,25 @@
 #  Requires an SSH key that is:
 #    1. Configured with GitHub
 #    2. Added to your SSH agent (see ssh-add -l)
-#
-#  Pushes with --tags and --all, but avoids --mirror
-#   (though I think --mirror *might* be better?)
+
+SLEEP_PERIOD=2
+
+[ -z "$GIT_TOP" ] && exit 1
+
+get_bad_commits() {
+  # List all bad commits in the repository: that is, those without author names and e-mails
+  git log --all --oneline --pretty=tformat:"%H %an:%aE" | \
+    sed -E -e '/\S* \S+:\S+/d' -e 's/(\S+) .*$/--no-contains \1/'
+}
+
+get_things_to_push() {
+  bad_commits=$(get_bad_commits | tr '\n' ' ')
+  [ -n "${bad_commits}" ] && echo "Skipping bad commits: ${bad_commits}" > /dev/stderr
+  # Branches without bad commits:
+  git for-each-ref --format='%(refname:short)' refs/heads/ ${bad_commits}
+  # Tags without bad commits:
+  git tag ${bad_commits}
+}
 
 for gitdir in ${GIT_TOP}/package/epics/modules/*.git ${GIT_TOP}/package/epics/base/base.git
 do
@@ -18,8 +34,15 @@ do
     (git remote get-url --push github-slac &> /dev/null) && (
         echo "* Updating $gitdir ..."
         # git push --dry-run --mirror github-slac
-        git push --all github-slac
-        git push --tags github-slac
+        to_push=$(get_things_to_push)
+        if [ -z "$to_push" ]; then
+          echo "Nothing to push. All refs have invalid commits?"
+          continue
+        fi
+        git push github-slac ${to_push} || sleep 5
+        # This may take a while, but we often get rate-limited as we hit GitHub
+        # quite a bit here:
+        sleep "${SLEEP_PERIOD}"
     ) || (
         echo "* $(basename $gitdir) is not on slac-epics"
     )
